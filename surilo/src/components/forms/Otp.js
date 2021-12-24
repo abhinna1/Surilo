4c3ddbc852f319123bcca454fe36c9d3dff30b78
utsavprz <Madridista0o>
2021-12-24 21:31:13 +0545

    Done with OTP prototype

--- surilo/src/components/forms/Otp.js
+++ surilo/src/components/forms/Otp.js
@@ -2,9 +2,44 @@ import './Otp.css';
 import React from 'react';
 import { useState } from 'react';
 
+
 const Otp = () => {
 
+    const [code,setCode] = useState({code1:"",code2:"",code3:"",code4:"",code5:"",code6:""})
+    
+    const [otp,setOtp] = useState();
 
+    const [enable,setEnable] = useState(true);
+    
+    const handleChange = (event) =>{
+        setCode({...code, [event.target.name]: event.target.value})
+    }
+
+    const sendOTP = (e) => {
+        e.preventDefault()
+        let otpValid = Math.floor(100000 + Math.random() * 900000)
+
+        console.log(otpValid)
+        setOtp(otpValid)
+        setEnable(false)
+    }
+
+    const g = otp;
+
+    const handleOTP = (e,getOtp) => {
+        e.preventDefault()
+
+        const usrCode = code.code1 + code.code2 + code.code3 + code.code4 + code.code5 + code.code6
+        console.log(usrCode)
+        console.log(getOtp)
+
+        if(usrCode == getOtp){
+            alert("Otp Valid")
+        }else{
+            alert("Invalid")
+        }
+    }
+    
     return ( 
 
         <div className="main-container d-flex justify-content-center align-items-center">
@@ -12,17 +47,21 @@ const Otp = () => {
             <div className="main-area d-flex flex-column align-items-center p-3">
                 <h4 className='title text-white'>Enter your OTP</h4>
                 <hr />
+            
+                <form>
+                <div className='otp-area d-flex justify-content-between' >
+                    <input className="box" autoComplete='off' type="text" name="code1" maxLength= "1" id="1" onChange={e => handleChange(e)}/>
+                    <input className="box" autoComplete='off' type="text" name="code2" maxLength= "1" id="2" onChange={e => handleChange(e)}/>
+                    <input className="box" autoComplete='off' type="text" name="code3" maxLength= "1" id="3" onChange={e => handleChange(e)}/>
+                    <input className="box" autoComplete='off' type="text" name="code4" maxLength= "1" id="4" onChange={e => handleChange(e)}/>
+                    <input className="box" autoComplete='off' type="text" name="code5" maxLength= "1" id="5" onChange={e => handleChange(e)}/>
+                    <input className="box" autoComplete='off' type="text" name="code6" maxLength= "1" id="6" onChange={e => handleChange(e)}/>
 
-                <div className='otp-area d-flex m-4 justify-content-between' >
-                    <input className="box" value= {code1} type="text" name="code1" maxLength= "1" id="1" />
-                    <input className="box" value= {code2} type="text" name="code2" maxLength= "1" id="2"/>
-                    <input className="box" value= {code3} type="text" name="code3" maxLength= "1" id="3" />
-                    <input className="box" value= {code4} type="text" name="code4" maxLength= "1" id="4" />
-                    <input className="box" value= {code5} type="text" name="code5" maxLength= "1" id="5" />
-                    <input className="box" value= {code6} type="text" name="code6" maxLength= "1" id="6" />
-                </div>
-
-                <button className='rounded p-1' onClick={e => handleCode(code,otpCode)}>Confirm</button>
+            </div>
+                <button disabled={enable} id="submitBtn" onClick = {e => handleOTP(e,g)} type='Submit' className='rounded p-1'>Confirm</button>
+                <button onClick={e => sendOTP(e)} className='rounded p-1'>Send Code</button>
+            </form>
+            
             </div>
 
         </div>
