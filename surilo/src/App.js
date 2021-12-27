d806bec8e4406ce19584e5d4cefbab89ffb121f9
Samyam412 <Kcsamy50@gmail.com>
2021-12-27 07:57:48 +0545

    login page shadow

--- surilo/src/App.js
+++ surilo/src/App.js
@@ -2,17 +2,18 @@ import './App.css';
 import MultipleInputs from './components/forms/multipleInputs';
 import InfoReg from './components/forms/InfoReg';
 import Otp from './components/forms/Otp'
+import Login from './components/forms/login';
 
 
 function App() {
   return (
     <div className="App">
       {/* <MultipleInputs></MultipleInputs> */}
-      <InfoReg></InfoReg>
+      {/* <InfoReg></InfoReg> */}
       {/* <Otp></Otp> */}
-
+        <Login></Login>
     </div>
-  );
+  );  
 }
 
 export default App;
