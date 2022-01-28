846247ac21ef271727407fa4d4e266ef5507f82a
abhinna1 <manandharabhinna@gmail.com>
2022-01-29 01:26:40 +0545

    backend-code-restructured.

--- backend/DbSetup.js
+++ backend/DbSetup.js
@@ -1,70 +1,32 @@
-const bcrypt = require('bcrypt');
+
+
+const mysql = require('mysql'); 
+require('dotenv').config();
+
 class DbSetup{
     constructor(){
-        const mysql = require('mysql');
         this.con = mysql.createConnection({
-            host:"localhost",
-            user:"root",
-            password:"#J@v@978",
-            database:"dbsurilo"
-        }
-        );
-    }
-    getConnection(){
-        return this.con;
-    }
-    insertUser(data){
-        this.con.connect((error)=>{
-            if(error){
-                console.log(error);
-            }
-            else{
-                console.log("connected");
-                var sql = "insert into tbl_user(firstName, lastName, dob, gender, username, email, password) values ?";
-                var values=[[data.firstName, data.lastName, data.dob, data.gender, data.username, data.email, data.password]];
-                this.con.query(sql, [values], 
-                    function (err, result) {
-                    if (err) throw err;
-                    console.log("Number of records inserted: " + result.affectedRows);
-                  });   
-            }
+            host:process.env.HOST,
+            user:process.env.USER,
+            password:process.env.PASSWORD,
+            database:process.env.DATABASE
         })
     }
 
-
-    async loginValidate(data){
-        let found = false ;
-        this.con.query(
-            "SELECT * FROM tbl_user WHERE email = ? AND password = ?",
-            [data.email,data.password],
-            (err,result) => {
-        
+    insertUser(data){
+        const sql = `insert into tbl_user (firstName, lastName, username, dob, gender, email, password) values ('${data.firstName}', '${data.lastName}', '${data.username}', '${data.dob}', '${data.gender}', '${data.email}', '${data.password}');`;
+        this.con.connect((err)=>{
             if(err){
                 console.log(err);
-                console.log("hiii");
-                found = false;
             }
-            else{
-
-            
-            
-            if (result.length == 1){
-                console.log(result.length);
-                
-                found = true;
-                }
-            else{
-                // console.log(result.RowDataPacket);
-                console.log("No User Found");
-                found = false;
-            }
-        }
-            
-            });
-        console.log("huuu");
-        return found;
-        
-
+            this.con.query(sql, (err, result)=>{
+                if(err) console.log(err);
+                console.log("New Sign Up Registered.");
+            })
+        })
     }
-}
+
+    
+} 
+
 module.exports = DbSetup;
--- backend/server.js
+++ backend/server.js
@@ -1,112 +1,32 @@
-
-const DbSetup = require('./DbSetup')
 const express = require('express');
+const DbSetup = require('./DbSetup');
+const bcrypt = require('bcrypt');
 
-const dotenv = require('dotenv');
-const path = require('path');
-const { OAuth2Client } = require('google-auth-library');
+const app = express();
+const db = new DbSetup();
 
-class Server{
-    constructor(){
-        this.db = new DbSetup();
-        this.app = express();
-        
-        this.regData = {firstName:"", lastName:"", dob:"", gender:"", username:"", email:"", password:""};
-        this.logData = {email:"",password:""};
-
-        this.app.use(express.json());
-        this.app.listen(8000, ()=>{console.log("Server Started On Port 8000!")});
-    }
-    getApp(){
-        return this.app;
-    }
-    listenRegister(){
-        this.app.post('/register',(req, res)=>{
-            this.regData.firstName =req.body.firstName;
-            this.regData.lastName =req.body.lastName;
-            this.regData.dob =req.body.dob;
-            this.regData.gender =req.body.gender;
-            this.regData.username =req.body.username;
-            this.regData.email =req.body.email;
-            this.regData.password =req.body.password;
-            try{
-                this.db.insertUser(this.regData);
-                console.log(this.regData);
-            }
-            catch(e){
-                console.log(e);
-            }   
-        });
-    }
-
-    listenLogin(){
-        this.app.post('/login',(req, res)=>{
-            this.logData.email =req.body.email;
-            this.logData.password =req.body.password;
-            try{
-                let verified = this.db.loginValidate(this.logData);
-                console.log(verified);
-            }
-            catch(e){
-                console.log(e);
-            }   
-        });
-
-    }
-}
+// Password Hashing Algorithm
+async function getHash(password, saltRounds = 10){
+    try {
+        const salt = await bcrypt.genSalt(saltRounds);
+        return await bcrypt.hash(password, salt);
     
+    } catch (error) {
+        console.log(error);
+    }
+    return null;
+};
+
+app.use(express.json());
 
 
-// //dotenv.config();
-// const client = new OAuth2Client('570589698753-g28a852qp3gr9cgrli2g7p4cotj7p89n.apps.googleusercontent.com');
+app.post('/register', async (req, res)=>{
+    console.log(`Password: ${req.body.password}`);
+    req.body.password = await getHash(req.body.password);
+    db.insertUser(req.body);
+    console.log(`Hashed Password: ${req.body.password}`);
 
-// const app = express();
-// app.use(express.json());
+});
 
-// const users = [];
+app.listen(process.env.PORT, (err)=>{if(err)console.log(err); else console.log(`Successfully connected to port ${process.env.PORT}.`)});
 
-// function upsert(array, item) {
-//   const i = array.findIndex((_item) => _item.email === item.email);
-//   if (i > -1) array[i] = item;
-//   else array.push(item);
-// }
-
-// app.post('/api/google-login', async (req, res) => {
-//   const { token } = req.body;
-//   const ticket = await client.verifyIdToken({
-//     idToken: token,
-//     audience: 570589698753-g28a852qp3gr9cgrli2g7p4cotj7p89n.apps.googleusercontent.com,
-//   });
-//   const { name, email, picture } = ticket.getPayload();
-//   upsert(users, { name, email, picture });
-//   res.status(201);
-//   res.json({ name, email, picture });
-// });
-
-// app.use(express.static(path.join(__dirname, '/build')));
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/build/index.html'))
-// );
-
-
-
-
-// function hashPassword(password){
-//     const salt = bcrypt.genSalt();
-//     salt.hashPassword()
-// }
-
-
-
-const serv = new Server();
-serv.listenRegister();
-
-serv.listenLogin();
-
-
-
-
-// app.post('/register',(req, res)=>{
-//     console.log(req.body.email);
-//     console.log(req.body.password);
-// });
